5eb663df6eafe302680c0a548ef9617f1fe755ad
yapni <yapni8@gmail.com>
2020-07-18 20:11:21 +0800

    Remove test code

--- main.py
+++ main.py
@@ -309,7 +309,6 @@ def main():
     job_queue.run_once(update_db, datetime.time(hour=0, minute=0, second=0, tzinfo=timezone)) # Every midnight
     job_queue.run_once(notify_user, datetime.time(hour=8, minute=30, second=0, tzinfo=timezone)) # Every 8.30am
     job_queue.run_repeating(wake, datetime.timedelta(minutes=10)) # Wake bot every 10 mins
-    job_queue.run_once(notify_user, datetime.timedelta(seconds=10))
 
     # Register callback functions
     # /start
